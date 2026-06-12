970c63d4d023d9a659fbd2942df10714a899191b
name <name@abc.com>
2026-06-12 15:02:12 -0400

    starter routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'books' },
+  /*
+  {
+    path: 'books',
+    loadComponent: () =>
+      import('./book-list/book-list').then((m) => m.BookList),
+  },
+  {
+    path: 'books/:id',
+    loadComponent: () =>
+      import('./book-detail/book-detail').then((m) => m.BookDetail),
+  },
+  {
+    path: 'books/:id/edit',
+    loadComponent: () =>
+      import('./book-edit/book-edit').then((m) => m.BookEdit),
+  },
+  */
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about').then((m) => m.About),
+  },
+  { path: '**', redirectTo: 'books' },
+];
