8e25a535d4f5ae3dd4f1a6e24d9d971bef1b800d
name <name@abc.com>
2026-06-12 14:42:23 -0400

    service methods

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -2,10 +2,28 @@ import { HttpClient, httpResource } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 
+import { Book } from './book';
+
 @Injectable({ providedIn: 'root' })
 export class BookService {
     private readonly http = inject(HttpClient);
     private readonly baseUrl = 'http://localhost:3000/books';
 
-    //
+    // GET all the books
+    booksResource() {
+        return httpResource<Book[]>(() => this.baseUrl, { defaultValue: [] });
+    }
+
+    // GET a single book
+    bookResource(id: () => number | undefined) {
+        return httpResource<Book>(() => {
+            const value = id();
+            return value == null ? undefined : `${this.baseUrl}/${value}`;
+        });
+    }
+
+    // PUT (update) a book
+    updateBook(book: Book): Promise<Book> {
+        return firstValueFrom(this.http.put<Book>(`${this.baseUrl}/${book.id}`, book));
+    }
 }
